4146d61b3d0bb02e367e496a1816656faeacc34c
anitaRead <areadman91@gmail.com>
2021-06-07 16:07:19 +0100

    Add touch_in, in_journey and touch_out functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,31 @@
 class Oystercard
     LIMIT = 90
-    attr_reader :balance
+    attr_reader :balance, :in_journey
+  
 
     def initialize
       @balance = 0
+      @in_journey = false
     end
 
     def top_up(amount)
       raise "unable to top-up: max top-up limit is £#{LIMIT}" if (amount + @balance) > LIMIT
       @balance += amount
     end
+
+    def deduct(fare)
+      @balance -= fare
+    end
+
+    def touch_in
+      @in_journey = true
+    end
+
+    def in_journey?
+      @in_journey
+    end
+
+    def touch_out
+      @in_journey = false
+    end 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -13,4 +13,29 @@ describe Oystercard do
     it 'raises an error if balance exceeds limit' do
       expect { subject.top_up(100) }.to raise_error 'unable to top-up: max top-up limit is £90'
     end
+
+    it 'deducts money from the balance' do
+      subject.top_up(50)
+      expect(subject.deduct(1.50)).to eq 48.50
+    end
+
+    it 'can touch in the oystercard' do
+      expect(subject.touch_in).to eq true
+    end
+
+    it 'shows user is in journey' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+    end
+
+    it 'can touch out the oystercard' do
+      expect(subject.touch_out).to eq false
+    end
+
+    it 'keeps track of when the user is in journey' do
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+      subject.touch_out
+      expect(subject.in_journey?).to eq false
+    end
 end
